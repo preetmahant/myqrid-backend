8a7d6bf752d713a29ba7fab65981e578ca38556e
preetmahant <preetmahant@gmail.com>
2026-05-09 11:06:57 +0530

    Create asyncHandler.js

---
+++ future-postgres/src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+function asyncHandler(fn) {
+
+  return (
+    req,
+    res,
+    next
+  ) => {
+
+    return Promise
+      .resolve(
+        fn(req, res, next)
+      )
+      .catch(next);
+  };
+}
+
+module.exports = {
+  asyncHandler
+};
