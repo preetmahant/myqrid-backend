288e2be36074aa8ac7399561a0a7557daa47f9e0
preetmahant <preetmahant@gmail.com>
2026-05-09 09:47:10 +0530

    Create subscription.controller.js

---
+++ future-postgres/src/controllers/subscription.controller.js
@@ -0,0 +1,43 @@
+const { prisma } = require(
+  "../config/prisma"
+);
+
+const { ok } = require(
+  "../utils/apiResponse"
+);
+
+async function list(req, res) {
+
+  const subscriptions =
+    await prisma.subscription.findMany({
+
+      take: 100,
+
+      orderBy: {
+        createdAt: "desc"
+      }
+    });
+
+  return ok(res, {
+    subscriptions
+  });
+}
+
+async function create(req, res) {
+
+  const subscription =
+    await prisma.subscription.create({
+      data: req.body
+    });
+
+  return ok(
+    res,
+    { subscription },
+    201
+  );
+}
+
+module.exports = {
+  list,
+  create
+};
