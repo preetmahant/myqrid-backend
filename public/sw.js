38546c4b7e3f9df7fdabb2f2b928cf1b64ebefe2
preetmahant <preetmahant@gmail.com>
2026-05-24 05:44:11 +0000

    fix: clear all SW cache

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,4 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+});
